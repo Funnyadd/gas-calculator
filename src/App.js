447dd0b022bc7ccfaa46cb65f8da84b6873ca976
Adam Mihajlovic <mihajlovicadam@gmail.com>
2023-07-20 01:00:06 -0400

    App is PWA ready

--- src/App.js
+++ src/App.js
@@ -32,40 +32,39 @@ const setField = (field, value) => {
         price /= form.numberOfPeople
 
     setField("answer", price)
-    console.log(form);
   };
 
   return (
     <>
       <div className='App d-flex flex-column align-items-center'>
-        <h1 className='mt-5 mb-5 text-warning' style={{'font-size':'44px'}}>Gas Price Splitter</h1>
+        <h1 className='mt-5 mb-5' style={{'font-size':'44px', 'color':'#00a3c9'}}>Gas Price Splitter</h1>
 
         <Form onSubmit={calculateSplitPrice}>
           <Form.Group className="mb-3" controlId="formBasicGasPrice">
             <Form.Label className="text-white">Gas price ( $ / L )</Form.Label>
-            <Form.Control size="lg" type="number" step="0.01" placeholder="enter price" value={form.gasPrice} onChange={e => setField("gasPrice", e.target.value)} />
+            <Form.Control size="lg" type="number" min={0.001} step="0.001" placeholder="enter price" value={form.gasPrice} onChange={e => setField("gasPrice", e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicAverageConsuption">
             <Form.Label className="text-white">Average consuption ( L / 100km )</Form.Label>
-            <Form.Control size="lg" type="number" step="0.1" placeholder="enter average" value={form.averageConsuption} onChange={e => setField("averageConsuption", e.target.value)} />
+            <Form.Control size="lg" type="number" min={0.1} step="0.1" placeholder="enter average" value={form.averageConsuption} onChange={e => setField("averageConsuption", e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicTripLength">
             <Form.Label className="text-white">Trip length ( km )</Form.Label>
-            <Form.Control size="lg" type="number" placeholder="enter length" value={form.tripLength} onChange={e => setField("tripLength", e.target.value)} />
+            <Form.Control size="lg" type="number" min={1} step="0.1" placeholder="enter length" value={form.tripLength} onChange={e => setField("tripLength", e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicNumberOfPeople">
             <Form.Label className="text-white">Number of people in the car</Form.Label>
-            <Form.Control size="lg" type="number" placeholder="enter number of people" value={form.numberOfPeople} onChange={e => setField("numberOfPeople", e.target.value)} />
+            <Form.Control size="lg" type="number" min={1} placeholder="enter number of people" value={form.numberOfPeople} onChange={e => setField("numberOfPeople", e.target.value)} />
           </Form.Group>
           
-          <Button className="mb-3 w-100 mt-3" style={{'font-size':'22px'}} variant="primary" type="submit">Calculate</Button>
+          <Button className="mb-3 w-100 mt-3" style={{'font-size':'22px', 'background-color':'#00a3c9', 'border-color':'#00a3c9'}} variant="primary" type="submit">Calculate</Button>
 
           <Form.Group className="mt-3" controlId="formBasicNumberOfPeople">
             <Form.Label className="text-white">Price per person: </Form.Label>
-            <Form.Text className="answerText text-success" style={{'font-size':'30px'}}> {form.answer.toFixed(2)} $</Form.Text>
+            <Form.Text className="answerText" style={{'font-size':'30px', 'color':'#00a3c9'}}> {form.answer.toFixed(2)} $</Form.Text>
           </Form.Group>
         </Form>
       </div>
